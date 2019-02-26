fbcca9da8fb10bb66715dd103c8201f85ffd9d56
Kenji Kihara <kenjikihara1220@gmail.com>
2019-02-26 10:07:40 +0900

    9まで

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello, world</h1>
-    );
+    return <h1>Hello, world</h1>;
   }
 }
 
